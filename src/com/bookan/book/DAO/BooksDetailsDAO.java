84038bedb8177145ab58362bf14ae71c58b83695
musaibasrar <musaibasrar@gmail.com>
2017-11-15 16:07:45 +0530

    Update BooksDetailsDAO.java

--- src/com/bookan/book/DAO/BooksDetailsDAO.java
+++ src/com/bookan/book/DAO/BooksDetailsDAO.java
@@ -7,9 +7,11 @@ import com.bookan.book.domain.BooksDetails;
 public interface BooksDetailsDAO {
 
 	public void addBooks(BooksDetails booksDetails);
-	public void updateBooks(BooksDetails booksDetails);
+	public String updateBookDetails(BooksDetails booksDetails);
 	public List<BooksDetails> listBooks();
 	public BooksDetails getBooksById(int id);
 	public List<BooksDetails> getAllBooks();
 	public String deleteBook(int id);
+	public BooksDetails getBookDetails(int id);
+	public List<BooksDetails> searchBooksByLanguage(String language);
 }
